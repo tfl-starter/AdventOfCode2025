ec436dfa6bb50857541b32c0e4fb9ad6ad5082f7
tfl-starter <t_flesch@web.de>
2025-12-26 10:28:49 +0100

    added basid unit tests

---
+++ src/Dial.java
@@ -0,0 +1,52 @@
+import java.nio.charset.UnsupportedCharsetException;
+
+public class Dial {
+    private int location = 50;
+
+    public int location() {
+        return location;
+    }
+
+    public void rotate(String rotation) {
+        int distance = distance(rotation);
+
+        if (isLeft(rotation)) { 
+            location += distance;
+        } else { 
+            location -= distance;
+        }
+        if (location > 100) {
+            location -= 100;
+        } 
+
+        if (location < 0){ 
+            location += 100;
+        }
+    }
+
+    public String direction(String rotation) {
+        String direction = "";
+        direction = rotation.substring(0,1);
+        
+        if (!direction.matches("[L,R]")) 
+           throw new UnsupportedCharsetException("Unexpected direction indicator");
+        return direction;
+    }
+
+    public int distance(String rotation) {
+        String distanceString = "";
+        distanceString = rotation.substring(1, rotation.length());
+
+        if (!distanceString.matches("[0-9]+")) 
+           throw new UnsupportedCharsetException("Unexpected distance value");
+
+        return Integer.valueOf(distanceString);
+    }
+
+    public boolean isLeft(String rotation) {
+        String direction = direction(rotation);
+        if (direction.startsWith("L")) return true;
+        return false;
+    }
+
+}
---
+++ src/DialTest.java
@@ -0,0 +1,72 @@
+import java.beans.Transient;
+import java.nio.charset.UnsupportedCharsetException;
+
+import org.junit.*;
+
+public class DialTest {
+    @Test
+    public void newDialReturnLocation50() {
+        Dial dial = new Dial();
+        Assert.assertEquals(50, dial.location());
+    }
+    @Test
+    public void isLeft() {
+        Dial dial = new Dial();
+        Assert.assertTrue(dial.isLeft("L5"));
+    }
+    @Test
+    public void newDialRotateLeft() {
+        Dial dial = new Dial();
+        dial.rotate( "L5");
+        Assert.assertEquals(55, dial.location());
+    }
+    @Test
+    public void newDialRotateRight() {
+        Dial dial = new Dial();
+        dial.rotate( "R10");
+        Assert.assertEquals(40, dial.location());
+    }
+    @Test
+    public void dialLeftOver99() {
+        Dial dial = new Dial();
+        dial.rotate( "L60");
+        Assert.assertEquals(10, dial.location());
+    }
+    @Test
+    public void dialRightBelowZero() {
+        Dial dial = new Dial();
+        dial.rotate( "R60");
+        Assert.assertEquals(90, dial.location());
+    }
+    @Test
+    public void directionLeft() {
+        Dial dial = new Dial();
+        Assert.assertEquals("L", dial.direction("L5"));
+        Assert.assertEquals(1, dial.direction("L5").length());
+    }
+    @Test
+    public void directionRight() {
+        Dial dial = new Dial();
+        Assert.assertEquals("R", dial.direction("R10"));
+    }
+    @Test(expected = UnsupportedCharsetException.class)
+    public void directionThrowsException() {
+        Dial dial = new Dial();
+        String direction = dial.direction("X12");
+    }
+    @Test
+    public void distanceLeft() {
+        Dial dial = new Dial();
+        Assert.assertEquals(5, dial.distance("L5"));
+    }
+    @Test
+    public void distanceRight() {
+        Dial dial = new Dial();
+        Assert.assertEquals(10, dial.distance("R10"));
+    }
+    @Test(expected = UnsupportedCharsetException.class)
+    public void distanceValueContainCharacters() {
+        Dial dial = new Dial();
+        int dummyDistance = dial.distance("XYZ");
+    }
+}
